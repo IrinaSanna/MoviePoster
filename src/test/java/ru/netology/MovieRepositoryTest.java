06ddfd279665826abad0909f541a3f7424ffd871
Irina D <RabotaYd2021@yandex.ru>
2024-03-14 18:03:41 +0300

    Added tests

--- src/test/java/ru/netology/MovieRepositoryTest.java
+++ src/test/java/ru/netology/MovieRepositoryTest.java
@@ -10,6 +10,8 @@ public class MovieRepositoryTest {
     MovieItem item3 = new MovieItem(3, "Отель 'Белград'", "комедия", 2021, 3);
     MovieItem item4 = new MovieItem(4, "Джентельмены", "боевик", 2020, 10);
     MovieItem item5 = new MovieItem(5, "Человек невидимка", "ужасы", 2018, 7);
+    MovieItem item6 = new MovieItem(6, "Тролли. Мировой тур", "мультфиль", 2011, 7);
+    MovieItem item7 = new MovieItem(7, "Номер один", "комедия", 2018, 5);
 
     @Test
     public void addingMovie() {
@@ -23,6 +25,74 @@ public class MovieRepositoryTest {
 
         MovieItem[] expected = {item1, item2, item3, item4, item5};
         MovieItem[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastMovieMoreStandard() {
+        MovieRepository repo = new MovieRepository(6);
+
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
+
+        MovieItem[] expected = {item7, item6, item5, item4, item3, item2};
+        MovieItem[] actual = repo.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastMovieMin() {
+        MovieRepository repo = new MovieRepository();
+
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+
+        MovieItem[] expected = {item3, item2, item1};
+        MovieItem[] actual = repo.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastMovieMax() {
+        MovieRepository repo = new MovieRepository();
+
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
+
+        MovieItem[] expected = {item7, item6, item5, item4, item3};
+        MovieItem[] actual = repo.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLastMovieAll() {
+        MovieRepository repo = new MovieRepository(8);
+
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+        repo.add(item4);
+        repo.add(item5);
+        repo.add(item6);
+        repo.add(item7);
+
+        MovieItem[] expected = {item7, item6, item5, item4, item3, item2, item1};
+        MovieItem[] actual = repo.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
